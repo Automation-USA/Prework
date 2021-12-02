f60c52f68453d6601b941aabdbfa164b76920ee9
Juio Toledo <projects@automationusa.net>
2021-12-02 12:22:45 -0500

    Initial commit

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipAmountLabel: UILabel!
+    @IBOutlet var tipControl: UISegmentedControl!
+    @IBOutlet var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.billAmountTextField.delegate = self
     }
 
-
+    // Dismiss keyboard on return/enter
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    //handle keyboard dismissal using tap gesture
+    @IBAction func dismissKeyboard (_sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        // Get bill amount form texts field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+        totalLabel.setNeedsDisplay()
+    }
+    
 }
 
